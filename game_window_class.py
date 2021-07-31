9a90f7ad18a59fd6e9fbd0beb280142b9d7710ab
dime.kai <jesus_kdm@hotmail.com>
2021-07-30 22:39:20 -0500

    Rules of Game of life added and reconstruct functions

--- game_window_class.py
+++ game_window_class.py
@@ -1,4 +1,5 @@
 import pygame
+import copy
 from cell_class import *
 
 vec = pygame.math.Vector2
@@ -15,10 +16,12 @@ class GameWindow:
         self.rows = self.width//self.CELL_SIZE
         self.cols = self.height//self.CELL_SIZE
         # Set up all cells in our grid in a random way
-        self.grid = [[Cell(self.image, x, y) for x in range(self.cols)] for y in range(self.rows)]
+        self.grid = self.__generate_grid()
+        
         for row in self.grid:
             for cell in row:
                 cell.get_neighbors(self.grid)
+
         self.color = {'DARK_GRAY' : (102, 102, 102)}
 
     def update(self):
@@ -33,7 +36,50 @@ class GameWindow:
         for row in self.grid:
             for cell in row:
                 cell.draw()
-
         self.window.blit(self.image, (self.pos.x, self.pos.y))
     
+
+    def reset_grid(self):
+        self.grid = self.__generate_grid()
+        for row in self.grid:
+            for cell in row:
+                cell.get_neighbors(self.grid)
+
+    def __generate_grid(self):
+        return [[Cell(self.image, x, y) for x in range(self.cols)] for y in range(self.rows)]
+
+    def evaluate(self):
+        '''
+        Each square has neighbors and rules that come with number of "live" neighbors.
+        1. Live cell with fewer than two live neighbors, then dead cell
+        2. Live cell with more than three live neighbors, then dead cell
+        3. Live cell with two or three live neighbors, then live cell
+        4. Live cell with three live neighbors, then live cell
+        '''
+        new_grid = copy.copy(self.grid)
+        
+        # Count how many neighbors are alive around of the current cell
+        for row in self.grid:
+            for cell in row:
+                cell.count_live_neighbors()
+        
+        for yidx, row in enumerate(self.grid):
+            for xidx, cell in enumerate(row):
+                if cell.alive:
+                    if cell.alive_neighbors == 2 or cell.alive_neighbors == 3:
+                        new_grid[yidx][xidx].alive = True
+                    if cell.alive_neighbors < 2 or cell.alive_neighbors > 3:
+                        new_grid[yidx][xidx].alive = False
+                else:
+                    if cell.alive_neighbors == 3:
+                        new_grid[yidx][xidx].alive = True
+
+        self.grid = new_grid
+
+
+
+
+
+            
+
         
